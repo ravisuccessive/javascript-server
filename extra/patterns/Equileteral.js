4ea4616affaea001fd9e01c63409c6e4633a45af
Ravi Shankar SIngh <72761001+ravisuccessive@users.noreply.github.com>
2020-10-20 10:08:30 +0530

    Update Equileteral.js

--- extra/patterns/Equileteral.js
+++ extra/patterns/Equileteral.js
@@ -1,29 +1,15 @@
-let x,a;
-n=process.argv[2]
-equilateral(n)
 function equilateral (x)
 {
     console.log("print a equilateral triangle with rows "+x);
-   
-    if(x<2 && x>10)
-       return(0);
-   
-    for(let i=1;i<=x;i++)
-     {
+    for(let i=1;i<=x;i++){
           a="";
-
-    for(let j=i;j<=x;j++)
-     {
+    for(let j=i;j<=x;j++){
           a+=" ";
     }
-
-    for (let z=i;z>0;z--)
-     {     
+    for (let z=i;z>0;z--){     
          a+="* ";
     }
-    
     console.log(a);
-
-     }
-
+    }
 }
+   equilateral(process.argv[2])
