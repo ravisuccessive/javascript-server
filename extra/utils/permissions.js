08ee80d3cc5646b3bf559742f8a6dd906cf15875
Ravi Singh <ravi.shankar@tr25.successivetech.com>
2020-10-22 16:48:44 +0530

    feat:permission updated

--- extra/utils/permissions.js
+++ extra/utils/permissions.js
@@ -1,18 +1,36 @@
-let permissions =
-{
-'getUsers': {
-all: ['head-trainer'],
-read : ['trainee', 'trainer'],
-write : ['trainer'],
-Delete: [],
+const permissions = {
+    'getUsers': {
+    all: ['head-trainer'],
+    read : ['trainee', 'trainer','reviewer'],
+    write : ['trainer'],
+    delete: [],
+    },
+    'getProduct': {
+    all: ['head-trainer'],
+    read : ['trainee', 'trainer'],
+    write : ['trainer'],
+    delete: ['manager'],
+    },
 }
+const hasPermissions = (moduleName, role, permissionType) =>{
+    if((Object.keys(permissions)).includes(moduleName)){
+        if (permissions[moduleName].all.includes(role)){
+            console.log(`${role} has ${permissionType} permissions`);
+            return true;
+        }
+        else 
+        if (permissions[moduleName][permissionType].includes(role)){
+            console.log(`${role} has ${permissionType} permissions`);
+            return true;
+        }
+        else{
+            console.log(`${role} does not have ${permissionType} permissions`);
+            return false;
+        }
+    }
+    else{
+        console.log(`${moduleName} is not a valid module`)
+    }
 }
 
-function hasPermission(moduleName,role,permissionType)
-{
-console.log(permissions[moduleName][permissionType].includes(role)); //using bracket operator
-
-}
-
-hasPermission('getUsers',"trainer","read");
-hasPermission('getUsers',"trainee","write");
+hasPermissions("getProduct", "trainer", "delete");
