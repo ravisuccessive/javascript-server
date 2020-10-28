c09bc561253cc130fa8cfba49744d04a160d9ed2
ravisuccessive <ravi.singh@successive.tech>
2020-10-28 19:44:44 +0530

    feat:HTTP Status code and handlers added

--- src/Server.ts
+++ src/Server.ts
@@ -1,4 +1,7 @@
 import * as express from "express";
+import * as bodyParser from "body-parser";
+import { notFoundHandler, errorHandler } from './libs/routes';
+
 class Server{
     app
     constructor(private config){
@@ -8,19 +11,46 @@ class Server{
         this.setupRouts()
         return this;
     }
-    setupRouts(){
+    public setupRouts(){
         const { app }=this;
-        app.get('/health-check',(req,res,next)=>{
+        app.use('/health-check',(req, res)=>{
+            console.log("inside Second middleware");
             res.send("I am OK");
         });
 
+        this.app.use(notFoundHandler);
+        this.app.use(errorHandler);
+        this.app.use((req, res, next) => {
+            next({
+                error: "Not Found",
+                code: 404
+                
+            })
+        })
+
+        this.app.use((err, req, res, next) => {
+            console.log(err);
+            res.json(
+                {
+                    "error ": err.error,
+                    status : err.code,
+                    message : err. message || "Error",
+                    timeStamp: new Date()
+                 
+                }
+            )
+        });
         return this;
+    }
+    public initBodyParser(){
+        this.app.use(bodyParser.json( {type : 'application/**json'}))
     }
     run(){
         const {app, config:{PORT}}=this;
         app.listen(PORT,(err)=>{
             if (err) {
                 console.log( err );
+                
             }
             console.log(`App is running on port ${PORT}`);
 
