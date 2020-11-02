eb4e4842c7d1f0ad595d8f6ad783159b6719a2dd
itadmin <itadmin@ravi.lapr218.successivetech.com>
2020-11-02 16:25:48 +0530

    feat:feature/39519 updated

--- src/Server.ts
+++ src/Server.ts
@@ -1,29 +1,27 @@
 import * as express from "express";
 class Server{
-    app
+    private app;
     constructor(private config){
         this.app=express()
     }
-    bootstrap(){
-        this.setupRouts()
+    bootstrap() {
+        this.setUpRoutes()
         return this;
     }
-    setupRouts(){
+    setUpRoutes() {
         const { app }=this;
-        app.get('/health-check',(req,res,next)=>{
+        app.get('/health-check',(req, res, next) => {
             res.send("I am OK");
         });
-
         return this;
     }
-    run(){
-        const {app, config:{PORT}}=this;
-        app.listen(PORT,(err)=>{
+    run() {
+        const {app, config: { port } }=this;
+        app.listen(port,(err) => {
             if (err) {
-                console.log( err );
+                console.log(err);
             }
-            console.log(`App is running on port ${PORT}`);
-
+            console.log(`App is running on port ${port}`);
         })
     }
 }
