37dfb06130b829760613919cebf2580801df02bd
ravisuccessive <ravi.singh@successive.tech>
2020-11-09 12:51:21 +0530

    feat:resolved conflicts

--- src/controllers/trainee/routes.ts
+++ src/controllers/trainee/routes.ts
@@ -1,12 +1,17 @@
 import { Router } from 'express';
 import traineeController from './controller';
+import validationHandler from '../../libs/routes/validationHandler';
+import Validation from './validation';
 
 const traineeRouter = Router();
 
 traineeRouter.route('/')
-    .get(traineeController.get)
-    .post(traineeController.create)
-    .put(traineeController.update)
-    .delete(traineeController.delete)
+.get(validationHandler(Validation.get), traineeController.get)
+.post(validationHandler(Validation.create), traineeController.create)
+.put(validationHandler(Validation.update), traineeController.update)
+.delete(validationHandler(Validation.Delete), traineeController.delete);
+
+traineeRouter.route('/:id')
+    .delete(validationHandler(Validation.Delete), traineeController.delete);
 
 export default traineeRouter;
